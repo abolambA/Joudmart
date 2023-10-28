3e53f08fe3d19ad5adf05b56c0ba5299f91bd844
abolambA <mn121529@gmail.com>
2023-10-28 20:24:19 +0400

    anything

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
   import 'package:flutter/material.dart';
-  // ignore: depend_on_referenced_packages
-  import 'package:firebase_core/firebase_core.dart';
   import 'package:joudmart/screens/arabic%20screens/intro_screen.dart';
 
-  Future<void> main() async{
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
-    runApp(joudmartApp());
-  }
+  void main() => runApp(joudmartApp());
 
   class joudmartApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
       return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'joudmart',
+      title: 'joudmart',
         theme: ThemeData(
           primarySwatch: Colors.green,
           visualDensity: VisualDensity.adaptivePlatformDensity,
