7d6a5f62689f717db0c0055fcb787a53ae75982e
abolambA <mn121529@gmail.com>
2024-03-30 23:13:14 +0400

    anything

--- lib/screens/arabic screens/login_screen.dart
+++ lib/screens/arabic screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:joudmart/screens/arabic%20screens/intro_screen.dart';
 import 'package:joudmart/screens/arabic%20screens/register_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 // ignore: must_be_immutable
 class LoginScreen extends StatefulWidget {
@@ -14,6 +15,8 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool missingField = false;
+  bool rememberMe = false;
+  String errorMessage = '';
 
   @override
   Widget build(BuildContext context) {
@@ -21,16 +24,15 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Theme(
         data: Theme.of(context).copyWith(
           textTheme: TextTheme(
-            bodyText1: TextStyle(color: Color(0xFF8CC63F)), // Set text color
-            bodyText2: TextStyle(color: Color(0xFF8CC63F)), // Set text color
-          ),
+            bodyText1: TextStyle(color: Color(0xFF8CC63F)),
+            bodyText2: TextStyle(color: Color(0xFF8CC63F)),
+          ),  
         ),
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 16.0), // Added padding
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Centered Title Text
               Align(
                 alignment: Alignment.topCenter,
                 child: Padding(
@@ -45,14 +47,12 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
-              // Email Input
               TextField(
                 controller: widget.emailController,
                 decoration: InputDecoration(
                   labelText: 'البريد الإلكتروني',
                 ),
               ),
-              // Password Input
               TextField(
                 controller: widget.passwordController,
                 decoration: InputDecoration(
@@ -60,72 +60,89 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
                 obscureText: true,
               ),
-              // Remember Me Checkbox
               Row(
                 children: [
-                  Checkbox(value: false, onChanged: (value) {}),
+                  Checkbox(
+                    value: rememberMe,
+                    onChanged: (value) {
+                      setState(() {
+                        rememberMe = value!;
+                      });
+                    },
+                  ),
                   Text('تذكرني'),
                 ],
               ),
-              // "ليس لديك حساب؟" Text
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('ليس لديك حساب؟'),
-                  SizedBox(width: 8), // Added SizedBox for spacing
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF8CC63F),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                    ),
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen()));
-                    },
-                    child: Text(
-                      'أنشئ حساب',
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16), // Added SizedBox for spacing
-              // Login Button with validation
+              SizedBox(height: 16),
               ElevatedButton(
                 onPressed: () async {
                   if (widget.emailController.text.isEmpty || widget.passwordController.text.isEmpty) {
                     setState(() {
                       missingField = true;
+                      errorMessage = 'الرجاء ملء جميع الحقول المطلوبة';
                     });
                   } else {
                     try {
                       setState(() {
                         missingField = false;
+                        errorMessage = '';
                       });
                       UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
                         email: widget.emailController.text,
                         password: widget.passwordController.text,
                       );
 
-                      // Redirect to the loading screen or any other screen upon successful login
                       if (userCredential.user != null) {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => LoadingScreen()));
+                        SharedPreferences prefs = await SharedPreferences.getInstance();
+                        await prefs.setString('email', widget.emailController.text);
+                        await prefs.setString('password', widget.passwordController.text);
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoadingScreen()));
                       }
                     } on FirebaseAuthException catch (e) {
-                      // Handle Firebase Authentication errors here (e.g., invalid credentials)
                       print(e.message);
+                      setState(() {
+                        errorMessage = 'عفواً, يبدو انه هنالك مشكلة في المعلومات المدخلة في حالة عدم وجود حساب قم بانشاء واحد في الاسفل';
+                      });
                     }
                   }
                 },
                 child: Text('تسجيل الدخول'),
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: missingField ? Colors.red : const Color(0xFF8CC63F),
+                  backgroundColor: missingField ? Colors.red : Color(0xFF8CC63F),
                 ),
               ),
-              if (missingField)
-                Text(
-                  'الرجاء ملء جميع الحقول المطلوبة',
+              if (errorMessage.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    errorMessage,
+                    style: TextStyle(color: Colors.red),
+                  ),
                 ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('ليس لديك حساب؟'),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF8CC63F),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => RegisterScreen()),
+                      );
+                    },
+                    child: Text('أنشئ حساب'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16), // Add SizedBox for spacing
             ],
           ),
         ),
--- lib/screens/arabic screens/register_screen.dart
+++ lib/screens/arabic screens/register_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:joudmart/screens/arabic%20screens/intro_screen.dart';
+import 'package:joudmart/screens/arabic%20screens/login_screen.dart';
 
 // ignore: must_be_immutable
 class RegisterScreen extends StatefulWidget {
@@ -16,13 +18,14 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
   bool missingField = false;
+  bool rememberMe = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 16.0), // Added padding
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -36,7 +39,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
-                      color: Color(0xFF8CC63F), // Set text color
+                      color: Color(0xFF8CC63F),
                     ),
                   ),
                 ),
@@ -73,17 +76,23 @@ class _RegisterScreenState extends State<RegisterScreen> {
               // Remember Me Checkbox
               Row(
                 children: [
-                  Checkbox(value: false, onChanged: (value) {}),
+                  Checkbox(
+                    value: rememberMe,
+                    onChanged: (value) {
+                      setState(() {
+                        rememberMe = value!;
+                      });
+                    },
+                  ),
                   Text(
                     'تذكرني',
                     style: TextStyle(
-                      color: Color(0xFF8CC63F), // Set text color
+                      color: Color(0xFF8CC63F),
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 16), // Added SizedBox for spacing
-              // Registration Button with validation
+              SizedBox(height: 16),
               ElevatedButton(
                 onPressed: () async {
                   if (widget.nameController.text.isEmpty ||
@@ -103,15 +112,15 @@ class _RegisterScreenState extends State<RegisterScreen> {
                         password: widget.passwordController.text,
                       );
                       if (userCredential.user != null) {
-                        // Save additional user data to Firestore
-                        await FirebaseFirestore.instance.collection('users').doc(userCredential.user?.uid).set({
+                        await FirebaseFirestore.instance.collection('users').doc(userCredential.user!.uid).set({
                           'name': widget.nameController.text,
                           'email': widget.emailController.text,
                           'phone': widget.phoneController.text,
                         });
-
-                        // Redirect to the loading screen or any other screen
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => LoadingScreen()));
+                        SharedPreferences prefs = await SharedPreferences.getInstance();
+                        await prefs.setString('email', widget.emailController.text);
+                        await prefs.setString('password', widget.passwordController.text);
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoadingScreen()));
                       }
                     } on FirebaseAuthException catch (e) {
                       print(e.message);
@@ -119,22 +128,45 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   }
                 },
                 child: Text(
-                  'إنشئ حساب',
+                  'أنشئ حساب',
                   style: TextStyle(
-                    color: Colors.white, // Set text color
+                    color: Color(0xFF2e3192),
                   ),
                 ),
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: missingField ? Colors.red : Color(0xFF8CC63F),
+                  backgroundColor: missingField ? Color(0xFF8CC63F) : Color(0xFF8CC63F),
                 ),
               ),
               if (missingField)
                 Text(
                   'الرجاء ملء جميع الحقول المطلوبة',
                   style: TextStyle(
-                    color: Colors.red, // Set text color
+                    color: Colors.red,
                   ),
                 ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('لديك حساب ؟'),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF8CC63F),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                      );
+                    },
+                    child: Text('سجل دخولك'),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
